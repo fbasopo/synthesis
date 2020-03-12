d023c2680833e49441aff0a6cacbd783950d0495
Arkanoid <rudo1ntandoyenkosi@gmail.com>
2020-03-12 17:16:21 +0200

    Final commit

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -27,20 +27,39 @@ let max a b =
 let ofTime h m s =
     (h * 3600) + (m * 60) + s
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime b =
+    match b < 0 with
+    | true -> (0,0,0)
+    | false ->
+    let h = b/3600
+    let m = (b % 3600)/60
+    let s =( b % 3600) % 60
+    (h,m,s)
 
-let digits _ =
-    failwith "Not implemented"
+
+let digits s=
+   let rec count i =
+       match i < 10 && i > -10 with
+       |true -> 1 
+       | false -> count(i/10) + 1
+   count s
 
 let minmax _ =
     failwith "Not implemented"
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap yr = match yr >= 1582 with
+    |false -> failwith "Invalid Year "
+    | true -> yr % 4= 0 && yr % 100 <> 0 || yr % 400 =0
+    |false -> false | true -> true
 
-let month _ =
-    failwith "Not implemented"
+let month m = match m < 1 || m > 12 with
+    | true -> failwith "Invalid month"
+    | false -> 
+              match m with
+              |1 -> ("January", 31) | 2 -> ("February", 28) | 3 ->  ("March", 31) | 4 -> ("April", 30)
+              |5 -> ("May", 31) | 6->  ("June", 30) | 7 ->  ("July", 31) | 8 ->("August", 31)
+              |9-> ("September", 30) |10 -> ("October", 31) | 11-> ("November", 30) | 12 -> ("December", 31)
+     
 
 let toBinary _ =
     failwith "Not implemented"
@@ -48,8 +67,10 @@ let toBinary _ =
 let bizFuzz _ =
     failwith "Not implemented"
 
-let monthDay _ _ =
-    failwith "Not implemented"
+let monthDay d y =
+       match d < 366 & d >0 || y <1582 with
+       |false  ->  failwith "Invalid Input"
+       |true -> 
 
 let coord _ =
     failwith "Not implemented"
